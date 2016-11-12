df8c7cb4ab8d97c80685d984c4a238acff32dc8e
nmaiese <masaniello12@gmail.com>
2016-11-12 10:46:19 +0100

    Basic Templates

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'nickname': 'Nello'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
---
+++ run.py
@@ -0,0 +1,5 @@
+#!venv/Scripts/python
+from app import app
+
+
+app.run(debug=True)
